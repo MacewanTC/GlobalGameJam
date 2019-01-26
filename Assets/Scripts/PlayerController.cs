75fd1dd157105dffb40f67b4c1ac6ad2b8b84df6
Teagan <Teagan@DESKTOP-HGDQKH8>
2019-01-25 22:51:11 -0700

    Adds Tilesets, a player controller, and sorting layers

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        body.MovePosition((Vector2)transform.position + (moveDirection.normalized * speed * Time.deltaTime));
+    }
+}
