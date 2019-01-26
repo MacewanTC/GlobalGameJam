8bc730ef4bf8e06bd1ec2b59c58794b6b80babb3
Teagan <Teagan@DESKTOP-HGDQKH8>
2019-01-25 23:25:24 -0700

    Adds game manager / Loss screen scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool finished = false;
+    public string nextScene;
+
+    void Update()
+    {
+        if (finished)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                EndGame();
+            }
+        }
+
+    }
+
+    void EndGame()
+    {
+        finished = true;
+        SceneManager.LoadScene(nextScene);
+    }
+}
