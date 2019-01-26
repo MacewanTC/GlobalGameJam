8fb8fae4c3b4dcd286c7dcc84c9fdbeaf4f4c894
Teagan <Teagan@DESKTOP-HGDQKH8>
2019-01-26 12:24:43 -0700

    Adds interaction dialogs

--- Assets/Scripts/TriggerRegenNeed.cs
+++ Assets/Scripts/TriggerRegenNeed.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerRegenNeed : MonoBehaviour
 {
@@ -8,6 +9,24 @@ public class TriggerRegenNeed : MonoBehaviour
     public float regenAmount = 0.5f;
     public float regenTime = 2.0f;
     public int need = 1; // See player controller :: ChangeNeed for enum
+
+
+    public Text text; 
+
+    void Start()
+    {
+        text.enabled = false;
+    }
+
+    void OnTriggerEnter2D()
+    {
+        text.enabled = true;
+    }
+
+    void OnTriggerExit2D()
+    {
+        text.enabled = false;
+    }
 
     void OnTriggerStay2D()
     {
