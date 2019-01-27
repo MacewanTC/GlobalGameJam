c3129a871178acd6ae465b73d84c289245589bad
FancifulEight <sja2@ualberta.ca>
2019-01-27 11:14:25 -0700

    Made Camera Prefab that Follows Player

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+	private PlayerController player;
+	private float distanceFromPlayerLayer = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+		player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -1*distanceFromPlayerLayer);
+    }
+}
