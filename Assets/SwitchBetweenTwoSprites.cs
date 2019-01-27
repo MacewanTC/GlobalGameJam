2effccd1c6dbf025edbfde641b488432f5c99593
FancifulEight <sja2@ualberta.ca>
2019-01-27 11:26:47 -0700

    Added Lose Screen

---
+++ Assets/SwitchBetweenTwoSprites.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBetweenTwoSprites : MonoBehaviour
+{
+	public Sprite spr1, spr2;
+	public float period = 1f;
+
+	private SpriteRenderer sprRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+		sprRend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if ( Time.time % period > period/2) {
+			sprRend.sprite = spr1;
+		} else {
+			sprRend.sprite = spr2;
+		}
+    }
+}
